b3d6c6fcdc588e474fab0d40bcc7120410780361
o-h <omaro_mail@hotmail.com>
2021-03-16 23:44:16 +0100

    Add timer to hub

--- Hubs/CryptoHub.cs
+++ Hubs/CryptoHub.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 
 namespace CryptoWatchAPI.Hubs
@@ -5,6 +6,7 @@ namespace CryptoWatchAPI.Hubs
     public class CryptoHub : Hub
     {
 
+        private static readonly System.Timers.Timer _timer = new System.Timers.Timer();
 
 
     }
